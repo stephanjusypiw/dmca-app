dd7685d70f3aea298fab6af8f8168a8e331967f0
Stephan Jusypiw <stephanj@internode.on.net>
2015-05-16 17:22:59 +1000

    Video 12 Complete
    Video 12 Complete

--- app/Http/Controllers/NoticesController.php
+++ app/Http/Controllers/NoticesController.php
@@ -29,7 +29,9 @@ class NoticesController extends Controller {
      */
     public function index()
     {
-        return $this->user->notices;
+        $notices = $this->user->notices;
+
+        return view('notices.index', compact('notices'));
     }
 
     /**
